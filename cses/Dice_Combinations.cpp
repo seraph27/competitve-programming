27d18c3629a2bd063191c446535868aa0dfa2628
Seraph <42664208+Seraph27@users.noreply.github.com>
2024-02-15 04:22:04 -0800

    resort

--- cses/Dice_Combinations.cpp
+++ cses/Dice_Combinations.cpp
@@ -9,26 +9,26 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 #define rint(l, r) uniform_int_distribution<int>(l, r)(rng)
 template<typename T> bool ckmin(T &a, const T &b) { return a > b ? a = b, 1 : 0; }
 template<typename T> bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
-
+const int mod = 1e9+7;
 const char nl = '\n';
 
 void test_case() {
-    int x, n; cin >> x >> n;
-    int ans = INT_MAX;
-    for(int i = 1; i < sqrt(x); i++){
-        if(x%i == 0 && x/i >= n){
-            ans = i;
-        } else{
-            cout << ans << nl;
-            return;
+    int n; cin >> n;
+    int dp[n+1];
+    fill(dp, dp+n+1, 0);
+    dp[0] = 1;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= 6 && i-j >= 0; j++){
+            (dp[i] += dp[i-j]) %= mod;
         }
     }
+    cout << dp[n] << nl;
 }
 
 int main() {    
     cin.tie(0)->sync_with_stdio(0);
     //freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
     int t = 1;
-    cin >> t;
+    //cin >> t;
     while (t--) test_case();
 }
