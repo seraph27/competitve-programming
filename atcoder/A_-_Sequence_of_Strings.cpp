e0dad01ad6711a62032ac4d838a6ba67a24517ef
Seraph <42664208+Seraph27@users.noreply.github.com>
2024-03-24 15:02:59 -0700

    update

--- atcoder/A_-_Sequence_of_Strings.cpp
+++ atcoder/A_-_Sequence_of_Strings.cpp
@@ -4,8 +4,7 @@
 using namespace std;
 
 int main(){
-	cin.tie(0)->sync_with_stdio(0);
-	
+    cin.tie(0)->sync_with_stdio(0);	
     int n;  cin >> n;
 
     vector<string> v(n);
---
+++ build.sh
@@ -0,0 +1,1 @@
+g++ -O2 -Wall -Wextra -Wshadow -o $1 $1.cpp
