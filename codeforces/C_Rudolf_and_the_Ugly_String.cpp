8a4efab132757ad297229431cc537643ad896a5f
Seraph <rexchao27@Gmail.com>
2024-04-03 22:46:48 -0700

    aaa

---
+++ codeforces/C_Rudolf_and_the_Ugly_String.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ar array
+#define db double
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)x.size()
+using namespace std;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define rint(l, r) uniform_int_distribution<int>(l, r)(rng)
+template<typename T> bool ckmin(T &a, const T &b) { return a > b ? a = b, 1 : 0; }
+template<typename T> bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.f); cerr << ','; __print(x.s); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+const int INF = 1001001001;
+const int mod = 1e9+7;
+const char nl = '\n';
+
+void test_case() {
+    int n; cin >> n;
+    string s; cin >> s;
+    string pie = "pie", map = "map", mapie = "mapie";
+    int a = 0, b = 0, c = 0;
+    int posa = 0, posb = 0, posc = 0;
+    while((posa = s.find(pie, posa)) != s.npos){
+        a++;
+        posa+=pie.length();
+    }
+    while((posb = s.find(map, posb)) != s.npos){
+        b++;
+        posb+=map.length();
+    }
+    while((posc = s.find(mapie, posc)) != s.npos){
+        c++;
+        posc+=mapie.length();
+    }
+    cout << a + b - c << nl;
+}
+
+int main() {    
+    cin.tie(0)->sync_with_stdio(0);
+    //freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+    int t = 1;
+    cin >> t;
+    while (t--) test_case();
+}
