e73b13d419ba62edf16fd26cb30e8e8b1b8ed6b9
Seraph27 <rexchao27@Gmail.com>
2024-04-19 02:12:16 -0700

    asd

---
+++ codeforces/C_Nene_s_Magical_Matrix.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ar array
+#define pb push_back
+#define fi first
+#define se second
+#define all(x) x.begin(), x.end()
+using namespace std;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define rint(l, r) uniform_int_distribution<int>(l, r)(rng)
+template<typename T> bool ckmin(T &a, const T &b) { return a > b ? a = b, 1 : 0; }
+template<typename T> bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.f); cerr << ','; __print(x.s); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+const ll INFLL = 100100100100100100;
+const int INF = 1001001001;
+const int mod = 1e9+7;
+const char nl = '\n';
+
+void test_case() {
+    int n; cin >> n;
+    
+    int x = (n-1)*2+1;
+    int ans = 0;
+    for(int i = n; i >=1; i--){
+        ans+=i*x;
+        x-=2;
+    }
+    cout << ans << " " << 2*n-1 << nl;
+    
+    string s = "";
+    for(int i = n; i >= 1; i--){
+        s+=to_string(i);
+        if(i!=1) s+=" ";
+    }
+
+    int row = 1;
+    for(int i = 0; i < 2*n-1; i++){
+        cout << (i%2==0 ? 1 : 2) << " ";
+        cout << row << " ";
+        if(i%2==0) row++;
+        cout << s << nl;
+    }
+
+
+}
+
+int main() {    
+    cin.tie(0)->sync_with_stdio(0);
+    //freopen("cownomics.in","r",stdin); freopen("cownomics.out","w",stdout);  //ucsao
+    int t = 1;
+    cin >> t;
+    while (t--) test_case();
+}
