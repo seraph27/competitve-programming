9385e1fc361fbd336d50e5a98ab54f66c983d3a0
Seraph <rexchao27@Gmail.com>
2024-05-09 13:21:02 -0700

    sad

--- cses/Apartments.cpp
+++ cses/Apartments.cpp
@@ -1,56 +1,63 @@
 #include <bits/stdc++.h>
 #define ll long long
 #define ar array
-#define db double
 #define all(x) x.begin(), x.end()
-#define sz(x) (int)x.size()
+#define pii pair<ll, ll>
 using namespace std;
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 #define rint(l, r) uniform_int_distribution<int>(l, r)(rng)
 template<typename T> bool ckmin(T &a, const T &b) { return a > b ? a = b, 1 : 0; }
 template<typename T> bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
 
+#ifdef SERAPH
+struct _debug {
+template<typename T> static void __print(const T &x) {
+    if constexpr (is_convertible_v<T, string> || is_fundamental_v<T>) cerr << x;
+    else { cerr << '{'; int f{}; for (auto i : x) cerr << (f++?",":""), __print(i); cerr << '}'; }
+}
+template<typename T, typename V>
+static void __print(const pair<T, V> &x) { cerr << '(', __print(x.first), cerr << ',', __print(x.second), cerr << ')'; }
+template<typename T, typename... V>
+static void _print(const T& t, const V&... v) { __print(t); if constexpr (sizeof...(v)) cerr << ", ", _print(v...); else cerr << "]\n"; }
+};
+#define debug(x...) cerr << "[" << #x << "] = [", _debug::_print(x)
+#else
+#define debug(x...)
+#endif
+
+const int mod = 1e9+7;
 const char nl = '\n';
+const int INF = 0x3f3f3f3f;
 
-void test_case() {
+void seraph() {
     int n, m, k; cin >> n >> m >> k;
-    vector<int> v1(n);
-    vector<int> v2(m); 
-
-    for(auto &a: v1){
-        cin >> a;
-    }
-
-    for(auto &a: v2){
-        cin >> a;
-    }
-
-    sort(all(v1));
-    sort(all(v2));
-
-    int ans = 0;
+    vector<ll> apply(n), apart(m);
+    for(auto&a: apply) cin >> a;
+    for(auto&a: apart) cin >> a;
+    sort(all(apply));
+    sort(all(apart));
     int i = 0, j = 0;
-    while(j < v2.size() && i < v1.size()){
-        //cout << i << " and " << j << nl;
-        if(abs(v2[j] - v1[i]) <= k){
-            ans++;
+    int ans = 0;
+    while(i < n && j < m) {
+        debug(apply[i], apart[j]);
+        if(apply[i] + k < apart[j]){
             i++;
-            j++;
-        } else if(v2[j] < v1[i]){
+        } else if(apply[i] - k > apart[j]){
             j++;
         } else{
             i++;
+            j++;
+            ans++;
         }
+        //debug(i, j, ans);
     }
-
     cout << ans << nl;
-
 }
 
-int main() {
+int main() {    
     cin.tie(0)->sync_with_stdio(0);
+    //freopen("perimeter.in","r",stdin); freopen("perimeter.out","w",stdout);
     int t = 1;
     //cin >> t;
-    while (t--) test_case();
+    while (t--) seraph();
 }
-
