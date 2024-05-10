ab2abb2c0f3e16d1bc95db01c6e465ba0458364f
Seraph27 <rexchao27@Gmail.com>
2024-05-10 16:15:42 -0700

    asd

--- cses/Concert_Tickets.cpp
+++ cses/Concert_Tickets.cpp
@@ -1,46 +1,57 @@
 #include <bits/stdc++.h>
 #define ll long long
 #define ar array
-#define db double
 #define all(x) x.begin(), x.end()
-#define sz(x) (int)x.size()
+#define pii pair<ll, ll>
 using namespace std;
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 #define rint(l, r) uniform_int_distribution<int>(l, r)(rng)
 template<typename T> bool ckmin(T &a, const T &b) { return a > b ? a = b, 1 : 0; }
 template<typename T> bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
 
+#ifdef SERAPH
+struct _debug {
+template<typename T> static void __print(const T &x) {
+    if constexpr (is_convertible_v<T, string> || is_fundamental_v<T>) cerr << x;
+    else { cerr << '{'; int f{}; for (auto i : x) cerr << (f++?",":""), __print(i); cerr << '}'; }
+}
+template<typename T, typename V>
+static void __print(const pair<T, V> &x) { cerr << '(', __print(x.first), cerr << ',', __print(x.second), cerr << ')'; }
+template<typename T, typename... V>
+static void _print(const T& t, const V&... v) { __print(t); if constexpr (sizeof...(v)) cerr << ", ", _print(v...); else cerr << "]\n"; }
+};
+#define debug(x...) cerr << "[" << #x << "] = [", _debug::_print(x)
+#else
+#define debug(x...)
+#endif
+
+const int mod = 1e9+7;
 const char nl = '\n';
+const int INF = 1001001001;
 
-void test_case() {
+void seraph() {
     int n, m; cin >> n >> m;
-    multiset<int> price;
     vector<int> maxprice(m);
-
+    multiset<int> price;
     for(int i = 0; i < n; i++){
         int a; cin >> a;
         price.insert(a);
     }
-
+    for(auto&a: maxprice) cin >> a;
     for(int i = 0; i < m; i++){
-        int t; cin >> t;
-        auto it = price.upper_bound(t);
-        if(it == price.begin()){
-            cout << -1 << nl;
-        } else{
+        auto it = price.upper_bound(maxprice[i]);
+        if(it == price.begin()) cout << -1 << nl;
+        else {
             cout << *(--it) << nl;
             price.erase(it);
         }
     }
-
-
 }
 
-int main() {
+int main() {    
     cin.tie(0)->sync_with_stdio(0);
+    //freopen("perimeter.in","r",stdin); freopen("perimeter.out","w",stdout);
     int t = 1;
     //cin >> t;
-    while (t--) test_case();
+    while (t--) seraph();
 }
-
-
--- cses/Movie_Festival.cpp
+++ cses/Movie_Festival.cpp
@@ -1,68 +1,56 @@
 #include <bits/stdc++.h>
 #define ll long long
 #define ar array
-#define db double
 #define all(x) x.begin(), x.end()
-#define sz(x) (int)x.size()
+#define pii pair<ll, ll>
 using namespace std;
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 #define rint(l, r) uniform_int_distribution<int>(l, r)(rng)
 template<typename T> bool ckmin(T &a, const T &b) { return a > b ? a = b, 1 : 0; }
 template<typename T> bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
 
-void __print(int x) {cerr << x;}
-void __print(long x) {cerr << x;}
-void __print(long long x) {cerr << x;}
-void __print(unsigned x) {cerr << x;}
-void __print(unsigned long x) {cerr << x;}
-void __print(unsigned long long x) {cerr << x;}
-void __print(float x) {cerr << x;}
-void __print(double x) {cerr << x;}
-void __print(long double x) {cerr << x;}
-void __print(char x) {cerr << '\'' << x << '\'';}
-void __print(const char *x) {cerr << '\"' << x << '\"';}
-void __print(const string &x) {cerr << '\"' << x << '\"';}
-void __print(bool x) {cerr << (x ? "true" : "false");}
+#ifdef SERAPH
+struct _debug {
+template<typename T> static void __print(const T &x) {
+    if constexpr (is_convertible_v<T, string> || is_fundamental_v<T>) cerr << x;
+    else { cerr << '{'; int f{}; for (auto i : x) cerr << (f++?",":""), __print(i); cerr << '}'; }
+}
 template<typename T, typename V>
-void __print(const pair<T, V> &x) {cerr << '{'; __print(x.f); cerr << ','; __print(x.s); cerr << '}';}
-template<typename T>
-void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
-void _print() {cerr << "]\n";}
-template <typename T, typename... V>
-void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
-#ifndef ONLINE_JUDGE
-#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+static void __print(const pair<T, V> &x) { cerr << '(', __print(x.first), cerr << ',', __print(x.second), cerr << ')'; }
+template<typename T, typename... V>
+static void _print(const T& t, const V&... v) { __print(t); if constexpr (sizeof...(v)) cerr << ", ", _print(v...); else cerr << "]\n"; }
+};
+#define debug(x...) cerr << "[" << #x << "] = [", _debug::_print(x)
 #else
 #define debug(x...)
 #endif
 
 const int mod = 1e9+7;
 const char nl = '\n';
+const int INF = 1001001001;
 
-void test_case() {
-    int n; cin >> n;;
-    vector<pair<int, int>> movies(n);
-    for(auto&w: movies){
+void seraph() {
+    int n; cin >> n;
+    vector<ar<int, 2>> vi(n);
+    for(int i = 0; i < n; i++){
         int a, b; cin >> a >> b;
-        w.first = b, w.second=a;
+        vi[i] = {b, a};
     }
-
-    sort(all(movies));
-    int ans = 1;
-    int currend = movies[0].first;
-    for(int i = 1; i < n; i++){
-        if(movies[i].second < currend) continue;
-        currend = movies[i].first;
-        debug(currend);
-        ans++;
+    sort(all(vi));
+    int ans = 0, lastend = 0;
+    for(int i = 0; i < n; i++){
+        if(vi[i][1] >= lastend){
+            ans++;
+            lastend = vi[i][0];
+        }
     }
     cout << ans << nl;
 }
 
 int main() {    
     cin.tie(0)->sync_with_stdio(0);
-    //freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+    //freopen("perimeter.in","r",stdin); freopen("perimeter.out","w",stdout);
     int t = 1;
     //cin >> t;
-    while (t--) test_case();
+    while (t--) seraph();
 }
