08eb7594e61b36597e16fc2ba034dc99fdc14362
Seraph27 <rexchao27@Gmail.com>
2024-09-29 17:27:55 -0700

    abc

---
+++ atcoder/abc328/e.cpp
@@ -0,0 +1,128 @@
+// Problem: $(PROBLEM)
+// Contest: $(CONTEST)
+// URL: $(URL)
+// Time Limit: $(TIMELIM)
+// Start: $(DATE)
+
+#include <bits/stdc++.h>
+#define sz(x) (int)x.size()
+#define ll long long
+#define ar array
+#define all(x) x.begin(), x.end()
+#define pii pair<ll, ll>
+#define pb push_back
+using namespace std;
+#define rint(l, r) uniform_int_distribution<int>(l, r)(rng)
+template<typename T> bool ckmin(T &a, const T &b) { return a > b ? a = b, 1 : 0; }
+template<typename T> bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+template<typename T, typename S> constexpr T ifloor(const T a, const S b){return a/b-(a%b&&(a^b)<0);}
+template<typename T, typename S> constexpr T iceil(const T a, const S b){return ifloor(a+b-1,b);}
+template<typename T> T isqrt(const T &x){T y=sqrt(x+2); while(y*y>x) y--; return y;}
+template<typename T>
+void sort_unique(vector<T> &vec){
+    sort(vec.begin(),vec.end());
+    vec.resize(unique(vec.begin(),vec.end())-vec.begin());
+}
+
+#ifdef MISAKA
+struct _debug {
+template<typename T> static void __print(const T &x) {
+    if constexpr (is_convertible_v<T, string> || is_fundamental_v<T>) cerr << x;
+    else { cerr << '{'; int f{}; for (auto i : x) cerr << (f++?",":""), __print(i); cerr << '}'; }
+}
+template<typename T, typename V>
+static void __print(const pair<T, V> &x) { cerr << '(', __print(x.first), cerr << ',', __print(x.second), cerr << ')'; }
+template<typename T, typename... V>
+static void _print(const T& t, const V&... v) { __print(t); if constexpr (sizeof...(v)) cerr << ", ", _print(v...); else cerr << "]\n"; }
+};
+#define debug(x...) cerr << "[" << #x << "] = [", _debug::_print(x)
+#else
+#define debug(x...)
+#endif
+struct DSU {
+    vector<int> f, siz;
+    
+    DSU() {}
+    DSU(int n) {
+        init(n);
+    }
+    
+    void init(int n) {
+        f.resize(n);
+        iota(f.begin(), f.end(), 0);
+        siz.assign(n, 1);
+    }
+    
+    int find(int x) {
+        while (x != f[x]) {
+            x = f[x] = f[f[x]];
+        }
+        return x;
+    }
+    
+    bool same(int x, int y) {
+        return find(x) == find(y);
+    }
+    
+    bool merge(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x == y) {
+            return false;
+        }
+        siz[x] += siz[y];
+        f[y] = x;
+        return true;
+    }
+    
+    int size(int x) {
+        return siz[find(x)];
+    }
+};
+const char nl = '\n';
+const int INF = 0x3f3f3f3f;
+
+void shiina_mashiro() { 
+    ll n, m, k; cin >> n >> m >> k;
+    vector<ar<ll, 3>> edges(m);
+    for(int i = 0; i < m; i++) {
+        int u, v; cin >> u >> v;
+        --u; --v;
+        ll w; cin >> w;
+        edges[i] = {u, v, w};
+    }
+    
+    vector<int> use;
+    ll ans = 1LL<<60;
+    auto dfs = [&](auto&&s, int u) -> void {
+        if(sz(use)==n-1) {
+            DSU uf(n);
+            ll cost = 0;
+            int bad = 0;
+            for(auto&e : use) {
+                if(uf.same(edges[e][0], edges[e][1])) bad = 1;
+                uf.merge(edges[e][0], edges[e][1]);
+                cost += edges[e][2];
+            }
+            if(!bad) ckmin(ans, cost%k);
+            return;
+        }
+        if(u == m) return;
+        s(s, u+1);
+        use.pb(u);
+        s(s, u+1);
+        use.pop_back();
+    };
+    dfs(dfs, 0);
+    cout << ans << nl;
+}
+
+int main() {    
+    cin.tie(0)->sync_with_stdio(0);
+    //freopen("perimeter.in","r",stdin); freopen("perimeter.out","w",stdout);
+    int t = 1;
+    //cin >> t;
+    while (t--) shiina_mashiro();
+}
+
+
