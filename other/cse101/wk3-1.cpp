d99586bde38642a31ddc468dfe42aa3090bb8c58
Seraph <rexchao27@Gmail.com>
2024-10-16 01:28:18 -0700

    asd

---
+++ other/cse101/wk3-1.cpp
@@ -0,0 +1,84 @@
+// Problem: $(PROBLEM)
+// Contest: $(CONTEST)
+// URL: $(URL)
+// Time Limit: $(TIMELIM)
+// Start: $(DATE)
+// mintemplate
+#include <bits/stdc++.h>
+#define sz(x) (int)x.size()
+#define ll long long
+#define ar array
+#define all(x) x.begin(), x.end()
+#define pii pair<ll, ll>
+#define pb push_back
+using namespace std;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define rint(l, r) uniform_int_distribution<int>(l, r)(rng)
+template<typename T> bool ckmin(T &a, const T &b) { return a >= b ? a = b, 1 : 0; }
+template<typename T> bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+template<typename T, typename S> constexpr T ifloor(const T a, const S b){return a/b-(a%b&&(a^b)<0);}
+template<typename T, typename S> constexpr T iceil(const T a, const S b){return ifloor(a+b-1,b);}
+template<typename T> T isqrt(const T &x){T y=sqrt(x+2); while(y*y>x) y--; return y;}
+template<typename T>
+void sort_unique(vector<T> &vec){
+    sort(vec.begin(),vec.end());
+    vec.resize(unique(vec.begin(),vec.end())-vec.begin());
+}
+
+#ifdef MISAKA
+struct _debug {
+template<typename T> static void __print(const T &x) {
+    if constexpr (is_convertible_v<T, string> || is_fundamental_v<T>) cerr << x;
+    else { cerr << '{'; int f{}; for (auto i : x) cerr << (f++?",":""), __print(i); cerr << '}'; }
+}
+template<typename T, typename V>
+static void __print(const pair<T, V> &x) { cerr << '(', __print(x.first), cerr << ',', __print(x.second), cerr << ')'; }
+template<typename T, typename... V>
+static void _print(const T& t, const V&... v) { __print(t); if constexpr (sizeof...(v)) cerr << ", ", _print(v...); else cerr << "]\n"; }
+};
+#define debug(x...) cerr << "[" << #x << "] = [", _debug::_print(x)
+#else
+#define debug(x...)
+#endif
+
+const char nl = '\n';
+const int INF = 0x3f3f3f3f;
+
+void shiina_mashiro() {
+    int n, m; cin >> n >> m;
+    int t0; cin >> t0;
+    int thresh; cin >> thresh;
+    vector<vector<pair<int, int>>> adj(n+1);
+    
+    for(int i = 0; i < m; i++) {
+        int u, v, t; cin >> u >> v >> t;
+        adj[u].pb({v, t});
+    }
+
+    vector<int> vtx(n+1, INF);
+    vtx[1] = t0;
+    priority_queue<ar<int, 3>, vector<ar<int, 3>>, greater<ar<int, 3>>> pq;
+    pq.push({t0, 1, 0}); //node, weight, dist;
+    while(!pq.empty()) {
+        auto [val, node, dist] = pq.top(); pq.pop();
+        if(val != vtx[node]) continue;
+        for(auto &[e, w] : adj[node]) {
+            if(w >= val && dist < thresh && ckmin(vtx[e], w)) {
+                pq.push({vtx[e], e, dist + 1});
+            }
+        }
+    }
+    for(int i = 1; i <= n; i++) cout << (vtx[i]==INF ? -1 : vtx[i]) << " ";
+    cout << nl;
+    
+}
+
+int main() {    
+    cin.tie(0)->sync_with_stdio(0);
+    //freopen("perimeter.in","r",stdin); freopen("perimeter.out","w",stdout);
+    int t = 1;
+    //cin >> t;
+    while (t--) shiina_mashiro();
+}
+
+
