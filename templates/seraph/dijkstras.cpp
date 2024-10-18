5c22394cebdb08d412e7926ee3befc0cc57829fa
Seraph27 <rexchao27@Gmail.com>
2024-10-18 15:47:44 -0700

    asd

--- templates/seraph/dijkstras.cpp
+++ templates/seraph/dijkstras.cpp
@@ -1,19 +1,16 @@
 template<typename T>
-vector<T> dijkstra(int s, vector<vector<pair<int,T>>> adj){
+vector<T> dijkstra(int start, vector<vector<pair<int,T>>> &adj){
     int n=sz(adj);
-    vector<T> dis(n,inf<T>);
-    vector<int> vis(n);
+    vector<T> dist(n,4e18); //change if needed
     priority_queue<pair<T,int>,vector<pair<T,int>>,greater<pair<T,int>>> pq;
-    dis[s]=0,pq.push({0,s});
+    dist[start]=0, pq.push({0,start});
     while(!pq.empty()){
-        auto [d,u]=pq.top(); pq.pop();
-        if(vis[u]) continue;
-        vis[u]=1;
-        for(auto &[v,w]: adj[u]) if(d+w<dis[v]){
-            dis[v]=d+w;
-            pq.push({dis[v],v});
+        auto [d,u] = pq.top(); pq.pop();
+        if(d != dist[u]) continue;
+        for(auto &[v,w]: adj[u]) if(ckmin(dist[v],d+w)){
+            pq.push({dist[v],v});
         }
     }
-    return dis;
+    return dist;
 }
 
