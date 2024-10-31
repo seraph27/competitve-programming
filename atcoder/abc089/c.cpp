ccec056a5582e7b4e6aa63a092c12b2a8c16dd89
Seraph <rexchao27@Gmail.com>
2024-10-30 20:27:45 -0700

    asd

---
+++ atcoder/abc089/c.cpp
@@ -0,0 +1,73 @@
+// Problem: C - March
+// Contest: AtCoder Beginner Contest 089
+// URL: https://atcoder.jp/contests/abc089/tasks/abc089_c
+// Time Limit: 2000
+// Start: 2024/10/30 17:01:45
+// mintemplate
+#include <bits/stdc++.h>
+#define sz(x) (int)x.size()
+#define ll long long
+#define ar array
+#define all(x) x.begin(), x.end()
+#define pii pair<ll, ll>
+#define pb push_back
+using namespace std;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define rint(l, r) uniform_int_distribution<int>(l, r)(rng)
+template<typename T> bool ckmin(T &a, const T &b) { return a > b ? a = b, 1 : 0; }
+template<typename T> bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+template<typename T, typename S> constexpr T ifloor(const T a, const S b){return a/b-(a%b&&(a^b)<0);}
+template<typename T, typename S> constexpr T iceil(const T a, const S b){return ifloor(a+b-1,b);}
+template<typename T> T isqrt(const T &x){T y=sqrt(x+2); while(y*y>x) y--; return y;}
+template<typename T>
+void sort_unique(vector<T> &vec){
+    sort(vec.begin(),vec.end());
+    vec.resize(unique(vec.begin(),vec.end())-vec.begin());
+}
+
+#ifdef MISAKA
+struct _debug {
+template<typename T> static void __print(const T &x) {
+    if constexpr (is_convertible_v<T, string> || is_fundamental_v<T>) cerr << x;
+    else { cerr << '{'; int f{}; for (auto i : x) cerr << (f++?",":""), __print(i); cerr << '}'; }
+}
+template<typename T, typename V>
+static void __print(const pair<T, V> &x) { cerr << '(', __print(x.first), cerr << ',', __print(x.second), cerr << ')'; }
+template<typename T, typename... V>
+static void _print(const T& t, const V&... v) { __print(t); if constexpr (sizeof...(v)) cerr << ", ", _print(v...); else cerr << "]\n"; }
+};
+#define debug(x...) cerr << "[" << #x << "] = [", _debug::_print(x)
+#else
+#define debug(x...)
+#endif
+
+const char nl = '\n';
+const int INF = 0x3f3f3f3f;
+
+void shiina_mashiro() {
+    int n; cin >> n;
+    vector<string> vi(n);
+    ar<int, 5> cnt{}; //m a r c h
+    for(auto&x : vi) {
+        cin >> x;
+        if(x[0]=='M') cnt[0]++;
+        if(x[0]=='A') cnt[1]++;
+        if(x[0]=='R') cnt[2]++;
+        if(x[0]=='C') cnt[3]++;
+        if(x[0]=='H') cnt[4]++;
+    }
+    ll ans = 0;
+    for(int i = 0; i < 5; i++) for(int j = i+1; j < 5; j++) for(int k = j+1; k < 5; k++) {
+        ans+=(ll)cnt[i]*cnt[j]*cnt[k];
+    }
+    cout << ans << nl;
+}
+
+int main() {    
+    cin.tie(0)->sync_with_stdio(0);
+    //freopen("perimeter.in","r",stdin); freopen("perimeter.out","w",stdout);
+    int t = 1;
+    //cin >> t;
+    while (t--) shiina_mashiro();
+}
+
