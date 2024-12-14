299a15c2f961ee7aff6da158a0b3cace662d7cfe
Seraph27 <rexchao27@Gmail.com>
2024-12-14 00:18:23 -0800

    asd

--- lib/dsa/segtree.cpp
+++ lib/dsa/segtree.cpp
@@ -15,6 +15,27 @@ struct segtree {
 };
 
 //bottom up ^
+struct segtree {
+    int n, vector<int> t;
+    segtree(int siz) {
+        n = siz;
+        t.assign(2 * n);
+        for(int i = n-1; i>=0; i--) t[i] = min(t[i<<1], t[i<<1|1])
+    }
+
+    void update(int p, int val) {
+        for(t[p+=n] = val; p > 1; p >>= 1) t[p>>1] = min(t[p], t[p^1]);
+    }
+
+    int query(int l, int r) {
+        int res = 4e18;
+        for(l += n, r += n; l < r; l >>= 1, r >>= 1) {
+            if(l&1) ckmin(res, t[l++]);
+            if(r&1) ckmin(res, t[--r]);
+        }
+        return res;
+    }
+};
 
 
 
