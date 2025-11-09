93ddb304c974fa9598e404cfe3e8bb8e0a9b8b39
Seraph27 <rexchao27@gmail.com>
2025-11-09 01:42:23 -0800

    asd

---
+++ other/icpc_club/2025_practice_1/m.cpp
@@ -0,0 +1,47 @@
+#ifdef MISAKA
+#define _GLIBCXX_DEBUG
+#endif
+
+#include <bits/stdc++.h>
+#define sz(x) (int)x.size()
+#define ar array
+#define all(x) x.begin(), x.end()
+#define pii pair<int, int>
+#define pb push_back
+#define eb emplace_back
+#define db double
+
+using namespace std;
+template<typename T> bool ckmin(T &a, const T &b) { return a > b ? a = b, 1 : 0; }
+template<typename T> bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+#ifdef MISAKA
+struct _debug {
+template<typename T, size_t N> static void __print(const T (&a)[N]) { cerr << '{'; for (size_t i = 0; i < N; ++i) { if (i) cerr << ',';__print(a[i]); }cerr << '}'; }
+template<typename T> static void __print(const T &x) {
+    if constexpr (is_convertible_v<T, string> || is_fundamental_v<T>) cerr << x;
+    else { cerr << '{'; int f{}; for (auto i : x) cerr << (f++?",":""), __print(i); cerr << '}'; }
+}
+template<typename T, typename V>
+static void __print(const pair<T, V> &x) { cerr << '(', __print(x.first), cerr << ',', __print(x.second), cerr << ')'; }
+template<typename T, typename... V>
+static void _print(const T& t, const V&... v) { __print(t); if constexpr (sizeof...(v)) cerr << ", ", _print(v...); else cerr << "]\n"; }
+};
+#define debug(x...) cerr << "[" << #x << "] = [", _debug::_print(x)
+#else
+#define debug(x...)
+#endif
+
+const char nl = '\n';
+
+void solve() {
+    int n, p, k; cin >> n >> p >> k;
+    
+}
+
+signed main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int t = 1;
+    //cin >> t;
+    while (t--) solve();
+}
